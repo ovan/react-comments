2d75dddfba774b1406e72f5885f617c9070a858b
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-08-23 14:07:16 +0300

    initial commit with comment model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReactComments::Application.config.secret_token = '12f04968fb1c25270977bbead278c1c17357a18f170a1f2f30bf72a2fbdb46a01f9e59ca107b952a17ce07ef6d79e20d812f38262f0341e36b339c450e11f16f'
